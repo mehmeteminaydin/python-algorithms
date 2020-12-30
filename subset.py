eafbeb67c009d0db6d6f1ecfe6038c5c119c711e
Mehmet Emin Aydın <meaydin.ceng@gmail.com>
2020-12-30 20:16:59 +0300

    Add files via upload

---
+++ subset.py
@@ -0,0 +1,11 @@
+# It creates the subset of the given list
+def subset(mylist):
+    if mylist == []:
+        return [[]]
+    return subset(mylist[:-1]) + helper(mylist[-1],subset(mylist[:-1]))
+
+# This function helps us to add element into the list items recursively
+def helper(element,lst):
+    if len(lst) == 0:
+        return []
+    return [lst[0]+[element]] + helper(element,lst[1:])
