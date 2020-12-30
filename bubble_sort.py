b5823c22e21c16925a14daa1a7b8a564e5ab8483
Mehmet Emin Aydın <meaydin.ceng@gmail.com>
2020-12-30 20:08:13 +0300

    Add files via upload
    Bubble sorting function with the help of while and for loops

---
+++ bubble_sort.py
@@ -0,0 +1,12 @@
+#It checks two elements of the given list
+#if the previous one is greater than the next one, they are replaced
+#When nothing is changed, it returns "bubble" sorted list
+def bubble_sorting(mylist):
+    another_tour = True
+    while another_tour:
+        another_tour = False
+        for i in range(len(mylist)-1):
+            if mylist[i] > mylist[i+1]:
+                mylist[i],mylist[i+1] = mylist[i+1],mylist[i]
+                another_tour=True
+    return mylist
